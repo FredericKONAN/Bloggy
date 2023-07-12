b79e52ed47807539fff695f8d939f0ac3f50e8fc
FredericKONAN <fredmoye.fk@gmail.com>
2023-07-12 14:37:48 +0000

    petit refactoring

--- src/EventSubscriber/EasyAdminEventSubscriber.php
+++ src/EventSubscriber/EasyAdminEventSubscriber.php
@@ -7,43 +7,45 @@ use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class EasyAdminEventSubscriber implements EventSubscriberInterface
 {
     public function __construct(private UserPasswordHasherInterface $passwordHasher){}
 
-    public function onBeforeEntityPersistedEvent(BeforeEntityPersistedEvent  $event): void
+    public function updatePassword(BeforeEntityPersistedEvent|BeforeEntityUpdatedEvent  $event): void
     {
-        $utilisateur = $event->getEntityInstance();
 
-        if (($utilisateur instanceof Utilisateur && $utilisateur->plainPassword )) {
-
-            $plainPassword = $utilisateur->plainPassword;
-            $mdpHash = $this->passwordHasher->hashPassword($utilisateur, $plainPassword);
-            $utilisateur->setPassword($mdpHash);
-        }
+        $this->hashpassword($event);
+//        $this->hashpasswordEvent($event->getEntityInstance());
+//        $utilisateur = $event->getEntityInstance();
+//
+//        if (($utilisateur instanceof Utilisateur && $utilisateur->plainPassword )) {
+//
+//            $plainPassword = $utilisateur->plainPassword;
+//            $mdpHash = $this->passwordHasher->hashPassword($utilisateur, $plainPassword);
+//            $utilisateur->setPassword($mdpHash);
+//        }
 
     }
 
-    public function onBeforeEntityUpdatedEvent(BeforeEntityUpdatedEvent $event): void
-    {
-        $utilisateur = $event->getEntityInstance();
-
-        if (($utilisateur instanceof Utilisateur && $utilisateur->plainPassword )) {
-
-            $plainPassword = $utilisateur->plainPassword;
-            $mdpHash = $this->passwordHasher->hashPassword($utilisateur, $plainPassword);
-            $utilisateur->setPassword($mdpHash);
-        }
-
-    }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            BeforeEntityPersistedEvent::class => 'onBeforeEntityPersistedEvent',
-            BeforeEntityUpdatedEvent::class => 'onBeforeEntityUpdatedEvent',
+            BeforeEntityPersistedEvent::class => 'updatePassword',
+            BeforeEntityUpdatedEvent::class => 'updatePassword',
         ];
+    }
+
+    private function hashPassword($event):void
+    {
+
+        $utilisateur = $event->getEntityInstance();
+        if (($utilisateur instanceof Utilisateur && $utilisateur->plainPassword )) {
+
+            $plainPassword = $utilisateur->plainPassword;
+            $mdpHash = $this->passwordHasher->hashPassword($utilisateur, $plainPassword);
+            $utilisateur->setPassword($mdpHash);
+        }
     }
 }
