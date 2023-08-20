b77c0aceac69f8b35f001e1c5bacf98e63175e84
FredericKONAN <fredmoye.fk@gmail.com>
2023-08-20 11:46:49 +0000

    link post and tag

---
+++ src/Controller/Admin/TagCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Tag;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class TagCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Tag::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            SlugField::new('slug', 'Slug')->setTargetFieldName('name'),
+        ];
+    }
+
+}
