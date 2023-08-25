40b899e9a9a428f8d8628ff0c7bf69e386e77470
FredericKONAN <fredmoye.fk@gmail.com>
2023-08-25 17:34:46 +0000

    Filter posts by tagSlug

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,16 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\CommentType;
-use App\Form\SharePostType;
 use App\Repository\CommentsRepository;
 use App\Repository\PostRepository;
+use App\Repository\TagRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
@@ -22,9 +19,27 @@ class PostController extends AbstractController
     public function __construct(private PostRepository $postRepository){}
 
     #[Route('/', name: 'app_accueil', methods: ['GET'])]
-    public function index(PaginatorInterface $paginator, Request $request): Response
+    #[Route(
+        '/tags/{slug}', name: 'app_post_by_tag',
+        requirements: [
+            'slug'=> Requirement::ASCII_SLUG,
+        ],
+        methods: ['GET']
+    )]
+    public function index(PaginatorInterface $paginator, Request $request, ?string $slug, TagRepository $tagRepository): Response
     {
-        $query = $this->postRepository->getAllPublishedOrderedQuery();
+        $tag = null;
+
+        if($slug){
+            $tag =  $tagRepository->findOneBySlug($slug);
+
+        }
+//        if($slugTag){
+//            dd('Post filtre par tag');
+//        }else{
+//            dd('Afficher tout');
+//        }
+        $query = $this->postRepository->getAllPublishedOrderedQuery($tag);
         $numeroDePage  = $request->query->getInt('page', 1);
 
 
@@ -32,7 +47,10 @@ class PostController extends AbstractController
             PaginatorInterface::PAGE_OUT_OF_RANGE=> 'fix',
         ]);
 
-        return $this->render('post/index.html.twig', compact('pagination'));
+        return $this->render('post/index.html.twig', [
+            'pagination' => $pagination,
+            'tagName' => $tag?->getName()
+        ]);
     }
 
     #[Route(
@@ -81,71 +99,71 @@ class PostController extends AbstractController
         return $this->render('post/show.html.twig', compact('post', 'comments', 'commentForm'));
     }
 
-    #[Route(
-        '/post/{slug}/partage',
-        name: 'app_post_share',
-        requirements: [
-//            'date' => Requirement::DATE_YMD,
-            'slug'=> Requirement::ASCII_SLUG,
-        ],
-        methods: ['GET', 'POST'],
-    )]
+//    #[Route(
+//        '/post/{slug}/partage',
+//        name: 'app_post_share',
+//        requirements: [
+////            'date' => Requirement::DATE_YMD,
+//            'slug'=> Requirement::ASCII_SLUG,
+//        ],
+//        methods: ['GET', 'POST'],
+//    )]
 //    #[Entity('post',expr: 'repository.findOneByPublishedDateAnSlug(date, slug)')]
-    public function sharePost(Request $request, MailerInterface $mailer, Post $post){
-
-
-//        $post = $this->postRepository->findOneByPublishedDateAnSlug($date,$slug);
-        $form = $this->createForm(SharePostType::class);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-           $data=  $form->getData();
-
-//            $postUrl = $this->generateUrl(
-//                'app_post_show',
-//                $post->getPathParams(),
-//                UrlGeneratorInterface::ABSOLUTE_URL
-//            );
-
-           $sujet = sprintf('%s vous recommande de lire "%s"', $data['nom_expediteur'], $post->getTitre());
-//           $message = sprintf(
+//    public function sharePost(Request $request, MailerInterface $mailer, Post $post){
 //
-//               "Lit \"%s\" a l'adresse suivant %s.\n\n voici le commentaire de %s < %s >",
-//               $post->getTitre(),
-//               $postUrl,
-//               $data['nom_expediteur'],
-//               $data['commentaires'],
 //
-//           );
-
-            $mail =  (new TemplatedEmail())
-                ->from(
-                    new Address(
-                        $this->getParameter('app.contact.email'),
-                        $this->getParameter('app.name'),
-                    ))
-
-                ->to($data['mail_destinateur'])
-                ->subject($sujet)
-                ->htmlTemplate('email/post/share.html.twig')
-                ->context([
-                    'nom_expediteur' => $data['nom_expediteur'],
-                    'post' => $post,
-                    'commentaire'=> $data['commentaires'],
-                ])
-            ;
-
-            $mailer->send($mail);
-
-            $this->addFlash('success', "l'article a ete partage avec succes avec votre amie.");
-
-            return $this->redirectToRoute('app_accueil');
-
-        }
-
-        return $this->render('post/share.html.twig', compact('form', 'post'));
-    }
+////        $post = $this->postRepository->findOneByPublishedDateAnSlug($date,$slug);
+//        $form = $this->createForm(SharePostType::class);
+//
+//        $form->handleRequest($request);
+//
+//        if($form->isSubmitted() && $form->isValid())
+//        {
+//           $data=  $form->getData();
+//
+////            $postUrl = $this->generateUrl(
+////                'app_post_show',
+////                $post->getPathParams(),
+////                UrlGeneratorInterface::ABSOLUTE_URL
+////            );
+//
+//           $sujet = sprintf('%s vous recommande de lire "%s"', $data['nom_expediteur'], $post->getTitre());
+////           $message = sprintf(
+////
+////               "Lit \"%s\" a l'adresse suivant %s.\n\n voici le commentaire de %s < %s >",
+////               $post->getTitre(),
+////               $postUrl,
+////               $data['nom_expediteur'],
+////               $data['commentaires'],
+////
+////           );
+//
+//            $mail =  (new TemplatedEmail())
+//                ->from(
+//                    new Address(
+//                        $this->getParameter('app.contact.email'),
+//                        $this->getParameter('app.name'),
+//                    ))
+//
+//                ->to($data['mail_destinateur'])
+//                ->subject($sujet)
+//                ->htmlTemplate('email/post/share.html.twig')
+//                ->context([
+//                    'nom_expediteur' => $data['nom_expediteur'],
+//                    'post' => $post,
+//                    'commentaire'=> $data['commentaires'],
+//                ])
+//            ;
+//
+//            $mailer->send($mail);
+//
+//            $this->addFlash('success', "l'article a ete partage avec succes avec votre amie.");
+//
+//            return $this->redirectToRoute('app_accueil');
+//
+//        }
+//
+//        return $this->render('post/share.html.twig', compact('form', 'post'));
+//    }
 
 }
